5277c6d8f747e9cb144ba6149c6a048ad77fe6d0
cquir <christian.quirouette@ryerson.ca>
2023-07-09 11:46:21 -0400

    finish sound

--- src/audio.ts
+++ src/audio.ts
@@ -1,7 +1,12 @@
 import * as THREE from "three";
+import state from "./state" 
 
 //@ts-ignore
 import backgroundMusicFile from "../public/assets/just-relax.mp3";
+
+//@ts-ignore
+import finishSoundFile from "../public/assets/finish.mp3";
+
 
 const loadBackgroundMusic = async (camera: THREE.PerspectiveCamera, listener: THREE.AudioListener ) => {
 	const backgroundMusic = new THREE.Audio(listener);
@@ -17,8 +22,24 @@ const loadBackgroundMusic = async (camera: THREE.PerspectiveCamera, listener: TH
 	});
   };
 
+const loadFinishSound = async (camera: THREE.PerspectiveCamera, listener: THREE.AudioListener ) => {
+	const finishSound = new THREE.Audio(listener);
+	const audioLoader = new THREE.AudioLoader();
+	return new Promise((resolve, reject) => {
+	  audioLoader.load(finishSoundFile, function (buffer) {
+		finishSound.setBuffer(buffer);
+		finishSound.setLoop(false);
+		finishSound.setVolume(1.0);
+		state.finishSound = finishSound;
+		resolve(null);
+	  });
+	});
+  };
+
+
 export async function loadAudio(camera: THREE.PerspectiveCamera) {
 	const listener = new THREE.AudioListener();
 	camera.add(listener);
 	await loadBackgroundMusic(camera,listener);
+	await loadFinishSound(camera,listener);
 };
